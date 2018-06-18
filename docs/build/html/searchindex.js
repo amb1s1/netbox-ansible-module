a482a26672f4f21e6213783e75efc2a71cc77cf7
amb1s1 <amb1s1@gmail.com>
2018-06-18 11:43:27 -0400

    Deleting DOC directory

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to netbox ansible\u2019s documentation!"],titleterms:{ansibl:0,document:0,indic:0,netbox:0,tabl:0,welcom:0}})
